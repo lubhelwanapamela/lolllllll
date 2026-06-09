a1790e374c0e2923cac9be425f08b86b00790a74
lubhelwanapamela <lubhelwanapamela@gmail.com>
2026-06-09 13:37:36 +0200

    Add files via upload

---
+++ main/java/com/example/weeklywheatertracker/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.weeklywheatertracker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnStart = findViewById<Button>(R.id.button)
+        val btnExit = findViewById<Button>(R.id.button2)
+
+        // Navigate to main screen
+        btnStart.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Exit App
+        btnExit.setOnClickListener {
+            finishAffinity()
+        }
+    }
+}
